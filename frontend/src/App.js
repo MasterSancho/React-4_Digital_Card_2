1cbd860da15fa47d60169c382cf6f4647e001d4e
Alex M <1maltsevalex@gmail.com>
2021-08-02 15:55:18 +0300

    02. 2.8.21

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,18 @@ const App = () => {
  return (
   <div>
    <h1>Welcome to Digital World</h1>
+   <img
+    className='showcase'
+    src='images/08.09.20_Arieli_Shay_030_png-min.png'
+    alt='image'
+   />
+   <div>
+    <img
+     className='profile'
+     src='images/08.09.20_Arieli_Shay_029-min.jpg'
+     alt=''
+    />
+   </div>
   </div>
  );
 };
